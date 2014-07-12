6e890870e96a6ee2ea921ffe98aeaf73cc2270c8
pi-georgia <georgia.inlo@gmail.com>
2014-07-12 18:04:27 +0300

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -6,8 +6,8 @@ md=data.table(mydata)
 small_md$V1<-as.Date(small_md$V1, "%d/%m/%y")
 small_md$V3<-as.numeric(small_md$V3)
 
-small_md<-md[md$V1 > "2/1/2007"),]
-small_md<-md[md$V1 < "2/2/2007"),]
+small_md<-md[md$V1 >= "2/1/2007"),]
+small_md<-md[md$V1 <= "2/2/2007"),]
 
 small_md$V3<-as.numeric(small_md$V3)
 
