2fe67ace6a32e241c8e9b984d8942393bc8c9240
pi-georgia <georgia.inlo@gmail.com>
2014-07-12 22:57:57 +0300

    Update plot2.r

--- plot2.r
+++ plot2.r
@@ -1,18 +1,33 @@
 filename<- "household_power_consumption.txt"
 
-thetab<- read.table(filename, sep=";", na.strings="?")
-library(data.table)
-md=data.table(thetab)
-small_md$V1<-as.Date(small_md$V1, "%d/%m/%y")
-small_md$V3<-as.numeric(small_md$V3)
+thetab<- read.table(filename, sep=";", na.strings="?", header=TRUE,  stringsAsFactors =!default.stringsAsFactors())
 
-small_md<-md[md$V1 >= "2/1/2007"),]
-small_md<-md[md$V1 <= "2/2/2007"),]
+#library(data.table)
 
-small_md$V3<-as.numeric(small_md$V3)
+md=data.frame(thetab)
+md<-droplevels(md)
 
+#convert types
+
+#dat<-strptime(paste(md$Date,md$Time,sep=" "),format='%d/%m/%Y %H:%M:%S')
+
+md$Date<-as.Date(md$Date, "%d/%m/%Y")
+md$Global_active_power<-as.numeric(md$Global_active_power)
+
+#subset
+
+small_md<-subset(md, Date >="2007/02/01", select=c(Date,Time, Global_active_power), na.omit())
+small_md<-subset(small_md, Date <= "2007/02/02", select=c(Date,Time, Global_active_power))
+                 
+theday<-strptime(paste(small_md$Date,small_md$Time,sep=" "),format="%Y-/%d-/%m %H:%M:%S")
+
+
+#plot
 library(datasets)
-plot(thetab,small_md$V3,type="l",xlab="",ylab="Global Active Power (kilowatts)")
+plot(theday,small_md$Global_active_power,type="l",xlab="",ylab="Global Active Power (kilowatts)")
+
+#create png
 dev.copy(png,file="plot2.png")
+dev.off()
 
 
